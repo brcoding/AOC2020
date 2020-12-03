be527e772079179013ff82a49ecff8a3ee42cde1
brcoding <barryruffner@gmail.com>
2020-12-03 09:02:25 -0800

    Day 3 part 2

--- day3/main.go
+++ day3/main.go
@@ -22,26 +22,53 @@ func main() {
 		input = append(input, scanner.Text())
 	}
 
-	rightIdx := 0
-
-	trees := 0
-	for idx, row := range input {
-		if idx == 0 {
-			continue
-		}
-		rightIdx += 3
-		if len(row) <= rightIdx {
-			rightIdx = rightIdx - len(row)
-		}
-		// tmpRow to print a pretty picture
-		tmpRow := []byte(row)
-		// # is a tree
-		if string(row[rightIdx]) == "#" {
-			tmpRow[rightIdx] = 'O'
-			trees++
-		}
-		tmpRow[rightIdx] = 'X'
-		fmt.Println(string(tmpRow))
+	slopes := [5][2]int{
+		{1, 1},
+		{3, 1},
+		{5, 1},
+		{7, 1},
+		{1, 2},
 	}
-	fmt.Printf("Trees Hit: %d\n", trees)
+
+	var slopeResults []int
+
+	for _, slope := range slopes {
+		fmt.Println(slope)
+		rightIdx := 0
+
+		trees := 0
+		for idx, row := range input {
+			if idx == 0 {
+				fmt.Println(row + " Skipped")
+				continue
+			}
+			if idx % slope[1] > 0 {
+				fmt.Println(row + " Skipped")
+				continue
+			}
+			rightIdx += slope[0]
+			if len(row) <= rightIdx {
+				rightIdx = rightIdx - len(row)
+			}
+			// tmpRow to print a pretty picture
+			tmpRow := []byte(row)
+			// # is a tree
+			if string(row[rightIdx]) == "#" {
+				tmpRow[rightIdx] = 'O'
+				trees++
+			}
+			tmpRow[rightIdx] = 'X'
+			fmt.Println(string(tmpRow))
+		}
+		slopeResults = append(slopeResults, trees)
+		trees = 0
+	}
+
+	totalTrees, productTrees :=	0, 1
+	for _, result := range slopeResults {
+		totalTrees += result
+		productTrees *= result
+	}
+	
+	fmt.Printf("Trees Hit: %d\nProduct: %d\n", totalTrees, productTrees)
 }
